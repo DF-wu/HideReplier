ba0a7e4ee0fd6e364f8494ad5171f5ea4aa5d08c
df <a2470abc@gmail.com>
2020-06-19 15:48:16 +0800

    add instruction to guide people where to find HideReplier

--- src/main/java/dfder/hidereplyer/service/DiscordService.java
+++ src/main/java/dfder/hidereplyer/service/DiscordService.java
@@ -87,12 +87,12 @@ public class DiscordService {
         em.makeEmbed(
                 recivedJSONofDiscordMessage.getUsername(),
                 discordFieldcontent,
-                "https://img.icons8.com/color/48/000000/drupal.png", // this post's link
+                "https://hidedbot.herokuapp.com/", // this post's link
                 hexColor,
                 null ,// new Embedobj.Footer("來自： " + ip,""),  // footer
-                new Embedobj.Thumbnail("https://i.imgur.com/LSlxNc7.png"), //機器人縮圖
+                new Embedobj.Thumbnail("https://i.imgur.com/7BH9WA2.jpg"), //機器人縮圖
                 new Embedobj.Image((String) recivedJSONofDiscordMessage.getExtra("imgUrl")),  //上傳圖片連結
-                new Embedobj.Author("匿名機器人v0.1", "https://hidedbot.herokuapp.com/","https://img.icons8.com/color/144/000000/drupal.png"),
+                new Embedobj.Author("匿名機器人v0.1(點我去發文)", "https://hidedbot.herokuapp.com/","https://img.icons8.com/color/144/000000/drupal.png"),
                 new Embedobj.Field("流水號", String.valueOf(sc.getCounter()) , true)
         );
         em.addField("來自： ", ip, true);
@@ -118,6 +118,9 @@ public class DiscordService {
                 );
         repo.insert(discordStoreData);
   
+        
+        recivedJSONofDiscordMessage.setAvatarUrl("https://img.icons8.com/color/288/000000/drupal.png");
+        
         // assign url
         recivedJSONofDiscordMessage.setUrl(PostMessage.NTOUCS_DISCORD_HATE);
         
