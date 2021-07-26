a762fd4916f88f7e2f5b159564e18f81ce05f3b4
dfder <a2470abc@gmail.com>
2021-07-26 17:17:12 +0800

    fix: fix cors issue and refactor with modern js syntax (thank @Stanley2058)

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,79 +1,73 @@
-const botNameInput = document.getElementById('botNameInput');
-const colorInput = document.getElementById('colorInput');
-const avatarInput = document.getElementById('avatarInput');
-const imageInput = document.getElementById('imageInput');
-const contentText = document.getElementById('contentText');
-const contentDiv = document.querySelector('.content');
+const botNameInput = document.getElementById("botNameInput");
+const colorInput = document.getElementById("colorInput");
+const avatarInput = document.getElementById("avatarInput");
+const imageInput = document.getElementById("imageInput");
+const contentText = document.getElementById("contentText");
+const contentDiv = document.querySelector(".content");
 
-const previewThumbnail = document.getElementById('previewThumbnail');
-const selectThumbnail = document.getElementById('selectThumbnail');
+const previewThumbnail = document.getElementById("previewThumbnail");
+const selectThumbnail = document.getElementById("selectThumbnail");
 
-const preview = document.getElementById('preview');
-const submit = document.getElementById('submit');
+const preview = document.getElementById("preview");
+const submit = document.getElementById("submit");
 
-const ipDisplay = document.getElementById('ipDisplay');
-const botNameDisplay = document.getElementById('botNameDisplay');
-const infoDisplay = document.getElementById('infoDisplay');
-const botNameWithColor = document.getElementById('botNameWithColor');
-const contentDisplay = document.getElementById('contentDisplay');
-const ipDisplayInContent = document.getElementById('ipDisplayInContent');
-const errorDisplay = document.getElementById('errorDisplay');
+const ipDisplay = document.getElementById("ipDisplay");
+const botNameDisplay = document.getElementById("botNameDisplay");
+const infoDisplay = document.getElementById("infoDisplay");
+const botNameWithColor = document.getElementById("botNameWithColor");
+const contentDisplay = document.getElementById("contentDisplay");
+const ipDisplayInContent = document.getElementById("ipDisplayInContent");
+const errorDisplay = document.getElementById("errorDisplay");
 
-const ipCountry = document.getElementById('ipCountry');
-const ipCity = document.getElementById('ipCity');
-const ipISP = document.getElementById('ipISP');
+const ipCountry = document.getElementById("ipCountry");
+const ipCity = document.getElementById("ipCity");
+const ipISP = document.getElementById("ipISP");
 
-const avatarImg = document.querySelector('.avatar img');
-const contentImgDiv = document.querySelector('.contentImgDiv');
-const contentImg = document.querySelector('.contentImgDiv img');
+const avatarImg = document.querySelector(".avatar img");
+const contentImgDiv = document.querySelector(".contentImgDiv");
+const contentImg = document.querySelector(".contentImgDiv img");
 
+const inputs = [botNameInput, colorInput, avatarInput, imageInput, contentText];
 
-const inputs = [
-    botNameInput,
-    colorInput,
-    avatarInput,
-    imageInput,
-    contentText
+const GetIpAPI = "https://httpbin.org/ip";
+const GetIpCountry = "https://ipwhois.app/json";
+const DefaultAvatarUrl =
+    "https://cdn.discordapp.com/avatars/710112845567623238/f377b595ef4e0ea17826d7afbb20633f.webp?size=128";
+const BackendUrl = window.location.origin + "/HideBot/discord";
+
+const thumbnails = [
+    "https://cdn.discordapp.com/attachments/408969877580414976/714868499994116096/kyaru02.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846090247635084/kyaru01.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846093959594014/kyaru04.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846094353858580/kyaru03.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846098371739728/kyaru08.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846100032946206/peko01.gif",
+    "https://cdn.discordapp.com/attachments/591643710454890505/723846103006445618/ue01.gif",
 ];
 
-
-
-
-const CorsAnyWhere = "https://thingproxy.freeboard.io/fetch/";
-const GetIpAPI = "https://httpbin.org/ip";
-const GetIpCountry = "http://ip-api.com/json"
-const DefaultAvatarUrl = "https://cdn.discordapp.com/avatars/710112845567623238/f377b595ef4e0ea17826d7afbb20633f.webp?size=128";
-const BackendUrl = window.location.origin + "/HideBot/discord";
 let currentIp = null;
+let thumbnailLink;
 
-
-const thumbnails =['https://cdn.discordapp.com/attachments/408969877580414976/714868499994116096/kyaru02.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846090247635084/kyaru01.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846093959594014/kyaru04.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846094353858580/kyaru03.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846098371739728/kyaru08.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846100032946206/peko01.gif',
-'https://cdn.discordapp.com/attachments/591643710454890505/723846103006445618/ue01.gif'];
-var thumbnailLink ;
-
-fetch(GetIpAPI).then(res => res.json()).then(res => {
-    const ip = res.origin.split(',')[0];
+const getIpInfo = async () => {
+    const { origin } = await fetch(GetIpAPI).then((res) => res.json());
+    const ip = origin.split(",")[0];
     currentIp = ipDisplay.innerHTML = ipDisplayInContent.innerHTML = ip;
-    
 
-    fetch(CorsAnyWhere + GetIpCountry + `/${currentIp}`).then(res => res.json()).then(res => {
-        ipCountry.innerHTML = res.country;
-        ipCity.innerHTML = res.city;
-        ipISP.innerHTML = res.isp;
-    });
-});
+    const { country, city, isp } = await fetch(
+        GetIpCountry + `/${currentIp}`
+    ).then((res) => res.json());
 
+    ipCountry.innerHTML = country;
+    ipCity.innerHTML = city;
+    ipISP.innerHTML = isp;
+};
+getIpInfo();
 
+if (localStorage.getItem("color"))
+    contentDiv.style.borderColor = colorInput.value =
+        localStorage.getItem("color");
 
-if (localStorage.getItem("color")) contentDiv.style.borderColor = colorInput.value = localStorage.getItem("color");
-
-
-function imgOnError(event) {
+const imgOnError = (event) => {
     if (event.target === avatarImg) {
         avatarImg.src = DefaultAvatarUrl;
         errorDisplay.innerHTML = "不支援的頭像連結";
@@ -81,9 +75,9 @@ function imgOnError(event) {
         contentImgDiv.hidden = true;
         errorDisplay.innerHTML = "不支援的圖片連結";
     }
-}
+};
 
-function previewOption() {
+const previewOption = () => {
     errorDisplay.innerHTML = "";
     if (!botNameInput.value || !contentText.value) {
         errorDisplay.innerHTML = "機器人名字以及內容不可為空";
@@ -109,16 +103,15 @@ function previewOption() {
     }
 
     //根據選項改預覽小圖
-    thumbnailLink = previewThumbnail.src = thumbnails[selectThumbnail.selectedIndex];
+    thumbnailLink = previewThumbnail.src =
+        thumbnails[selectThumbnail.selectedIndex];
+};
 
-}
+const parseBBCode = (str) => {
+    return str.replace("[img]", "").replace("[/img]", "").trim();
+};
 
-function parseBBCode(str) {
-    return str.replace("[img]","").replace("[/img]","").trim();
-}
-
-function submitPost() {
- 
+const submitPost = async () => {
     const postBody = {
         username: botNameInput.value,
         content: contentText.value,
@@ -126,23 +119,26 @@ function submitPost() {
         avatar_url: avatarInput.value,
         imgUrl: imageInput.value,
         ip: currentIp,
-        thumbnail: thumbnailLink
+        thumbnail: thumbnailLink,
     };
 
     localStorage.setItem("color", colorInput.value);
 
-    fetch(BackendUrl, {
+    await fetch(BackendUrl, {
         body: JSON.stringify(postBody),
-        method: 'POST',
-        mode: 'cors',
+        method: "POST",
+        mode: "cors",
         headers: {
-            "content-type": "application/json"
-        }
-    }).then(res => res.json()).then(res => alert("發送成功!!"));
-}
+            "content-type": "application/json",
+        },
+    });
+    alert("發送成功!!");
+};
 
-inputs.forEach(inp => inp.addEventListener('keydown', () => submit.disabled = true));
+inputs.forEach((inp) =>
+    inp.addEventListener("keydown", () => (submit.disabled = true))
+);
 avatarImg.onerror = imgOnError;
 contentImg.onerror = imgOnError;
-preview.addEventListener('click', previewOption);
-submit.addEventListener('click', submitPost);
+preview.onclick = previewOption;
+submit.onclick = submitPost;
