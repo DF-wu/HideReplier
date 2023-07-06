322f14d0b44c8ca4a80b65565231cf0120991c97
dfder <a2470abc@gmail.com>
2023-07-06 12:45:53 +0800

    fix: change application.properties value injection way.

--- src/main/java/dfder/hidereplyer/configure/MyConfig.java
+++ src/main/java/dfder/hidereplyer/configure/MyConfig.java
@@ -1,43 +1,33 @@
 package dfder.hidereplyer.configure;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "hidereplier")
+//@ConfigurationProperties(prefix = "hidereplier")
 public class MyConfig {
-    private static String version;
-    private static String hostURL;
 
-    private static String DCWebhook;
+    @Value("${hidereplier.version}")
+    private String version;
 
-    public static String getDCWebhook() {
-        return DCWebhook;
-    }
+    @Value("${hidereplier.hostURL}")
+    private String hostURL;
 
-    public static void setDCWebhook(String DCWebhook) {
-        MyConfig.DCWebhook = DCWebhook;
+    @Value("${hidereplier.DCWebhook}")
+    private String DCWebhook;
+
+    public String getVersion() {
+        return version;
     }
 
     public String getHostURL() {
         return hostURL;
     }
 
-    public void setHostURL(String hostURL) {
-        MyConfig.hostURL = hostURL;
-    }
-    //-------------getter & setter start-----------
-    
-    public String getVersion()
-    {
-        return version;
-    }
-    
-    public void setVersion(String version)
-    {
-        this.version = version;
+    public String getDCWebhook() {
+        return DCWebhook;
     }
 
-    
-    //-------------getter & setter end-----------
+//-------------getter & setter end-----------
 }
